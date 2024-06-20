a7cd6f46c6fbc8c9766dbadd04ff2b62990689cb
Apurva Anand <apurvanand29@gmail.com>
2024-06-19 22:48:12 -0500

    Adding comment for better understanding different cases

--- BasicMathLogic/ReverseInteger/ReverseInteger.py
+++ BasicMathLogic/ReverseInteger/ReverseInteger.py
@@ -1,15 +1,20 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        # Initialized variable to store reversed number
         rev = 0
         
+        # Case: Positive number
         if x>0:
             while x!=0:
+                # Extracting the last digit of x
                 num = x%10
                 rev = rev * 10 + num
                 x=x//10
             if rev < -2**31 or rev > 2**31 - 1:
                 return 0
             else: return rev
+       
+        # Case: Negative number
         elif x<0:
             x = abs(x)
             while x!=0:
