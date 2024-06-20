ea1eabd1720e399ae0aaead2f3ba022b44da8af0
Apurva Anand <apurvanand29@gmail.com>
2024-06-19 22:49:30 -0500

    Added comment for extracting, reversing and removing

--- BasicMathLogic/ReverseInteger/ReverseInteger.py
+++ BasicMathLogic/ReverseInteger/ReverseInteger.py
@@ -8,8 +8,13 @@ class Solution:
             while x!=0:
                 # Extracting the last digit of x
                 num = x%10
+                
+                # Reversing number
                 rev = rev * 10 + num
+
+                # Removing the last digit from x
                 x=x//10
+                
             if rev < -2**31 or rev > 2**31 - 1:
                 return 0
             else: return rev
